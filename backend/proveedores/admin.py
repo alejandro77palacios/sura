601303ee29cc1eb372296713bb0b7fced705f5d0
alejandro77palacios <alex_palacios77@ciencias.unam.mx>
2024-09-07 19:30:46 -0600

    Habilitar DjangoAdmin para los modelos creados

--- backend/proveedores/admin.py
+++ backend/proveedores/admin.py
@@ -1,3 +1,43 @@
 from django.contrib import admin
 
-# Register your models here.
+from proveedores.models import Proveedor, Producto, ProductoEnSolicitud, SolicitudCompra
+
+
+@admin.register(Proveedor)
+class ProveedorAdmin(admin.ModelAdmin):
+    list_display = ("id", "nombre", "direccion", "telefono", "correo")
+
+
+@admin.register(Producto)
+class ProductoAdmin(admin.ModelAdmin):
+    list_display = ("id", "nombre", "formatear_precio", "proveedor")
+    search_fields = ("nombre", "precio")
+    list_filter = ("proveedor",)
+
+    def formatear_precio(self, obj):
+        return f"${obj.precio:,.2f}"
+
+    formatear_precio.short_description = "PRECIO"
+
+
+class ProductoEnSolicitudInline(admin.TabularInline):
+    model = ProductoEnSolicitud
+    extra = 0
+    fields = ('producto', 'cantidad')
+
+
+@admin.register(SolicitudCompra)
+class SolicitudCompraAdmin(admin.ModelAdmin):
+    list_display = ("id", "formatear_fecha", "formatear_total", "aprobada")
+    inlines = [ProductoEnSolicitudInline]
+    fields = ("aprobada",)
+
+    def formatear_total(self, obj):
+        return f"${obj.total:,.2f}"
+
+    formatear_total.short_description = "TOTAL"
+
+    def formatear_fecha(self, obj):
+        return obj.fecha.strftime("%d %b %Y")
+
+    formatear_fecha.short_description = "FECHA"
