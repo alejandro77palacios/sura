414886b43eb89d302d3683b2957844d06e4582da
alejandro77palacios <alex_palacios77@ciencias.unam.mx>
2024-09-09 10:34:48 -0600

    Añadir App.js

---
+++ frontend/src/App.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Inicio from './comun/Inicio';
+import ProveedorList from './proveedor/ProveedorList';
+import ProveedorDetail from './proveedor/ProveedorDetail';
+import ProductoList from './productos/ProductoList';
+import ProductoDetail from './productos/ProductoDetail';
+import ProveedorForm from './proveedor/ProveedorForm';
+import ProductoForm from './productos/ProductoForm';
+import ProductoEnSolicitudDetail from './productos_en_solicitudes/ProductoEnSolicitudDetail';
+import ProductoEnSolicitudForm from './productos_en_solicitudes/ProductoEnSolicitudForm';
+import SolicitudCompraForm from './solicitudes/SolicitudCompraForm';
+import SolicitudCompraList from './solicitudes/SolicitudCompraList';
+import SolicitudCompraDetail from './solicitudes/SolicitudCompraDetail';
+import BotonInicio from './comun/BotonInicio';
+import './App.css';
+
+function App() {
+  return (
+    <Router>
+      <div>
+        <BotonInicio />
+        <Routes>
+          <Route exact path="/" element={<Inicio />} />
+          <Route path="/proveedores" element={<ProveedorList />} />
+          <Route path="/proveedor/:id" element={<ProveedorDetail />} />
+          <Route path="/registrar-proveedor" element={<ProveedorForm />} />
+
+          <Route path="/productos" element={<ProductoList />} />
+          <Route path="/producto/:id" element={<ProductoDetail />} />
+          <Route path="/registrar-producto" element={<ProductoForm />} />
+
+          <Route path="/producto-en-solicitud/:id" element={<ProductoEnSolicitudDetail />} />
+          <Route path="/registrar-producto-en-solicitud" element={<ProductoEnSolicitudForm />} />
+
+          <Route path="/solicitudes" element={<SolicitudCompraList />} />
+          <Route path="/solicitud/:id" element={<SolicitudCompraDetail />} />
+          <Route path="/registrar-solicitud" element={<SolicitudCompraForm />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
