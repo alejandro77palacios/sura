bca37a3996cde2f7fc3af3327e0a671b24f9f511
alejandro77palacios <alex_palacios77@ciencias.unam.mx>
2024-09-09 09:56:31 -0600

    Añadir vistas de modelos

--- backend/proveedores/views.py
+++ backend/proveedores/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from .models import Proveedor, Producto, SolicitudCompra, ProductoEnSolicitud
+from .serializers import ProveedorSerializer, ProductoSerializer, SolicitudCompraSerializer, \
+    ProductoEnSolicitudSerializer
+
+
+class ProveedorView(viewsets.ModelViewSet):
+    queryset = Proveedor.objects.all()
+    serializer_class = ProveedorSerializer
+
+
+class ProductoView(viewsets.ModelViewSet):
+    queryset = Producto.objects.all()
+    serializer_class = ProductoSerializer
+
+
+class ProductoEnSolicitudViewSet(viewsets.ModelViewSet):
+    queryset = ProductoEnSolicitud.objects.all()
+    serializer_class = ProductoEnSolicitudSerializer
+
+
+class SolicitudCompraViewSet(viewsets.ModelViewSet):
+    queryset = SolicitudCompra.objects.all()
+    serializer_class = SolicitudCompraSerializer
