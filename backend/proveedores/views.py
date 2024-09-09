54e63d691c79b34148d23330ae0eb3df824267dd
alejandro77palacios <alex_palacios77@ciencias.unam.mx>
2024-09-09 17:20:07 -0600

    Controlar usuarios en vistas

--- backend/proveedores/views.py
+++ backend/proveedores/views.py
@@ -1,8 +1,17 @@
-from rest_framework import viewsets
+from django.contrib.auth import get_user_model
+from rest_framework import viewsets, generics
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
 from .models import Proveedor, Producto, SolicitudCompra, ProductoEnSolicitud
-from .serializers import ProveedorSerializer, ProductoSerializer, SolicitudCompraSerializer, \
-    ProductoEnSolicitudSerializer
+from .serializers import (
+    ProveedorSerializer,
+    ProductoSerializer,
+    SolicitudCompraSerializer,
+    ProductoEnSolicitudSerializer,
+    UserSerializer,
+)
 
 
 class ProveedorView(viewsets.ModelViewSet):
@@ -23,3 +32,22 @@ class ProductoEnSolicitudViewSet(viewsets.ModelViewSet):
 class SolicitudCompraViewSet(viewsets.ModelViewSet):
     queryset = SolicitudCompra.objects.all()
     serializer_class = SolicitudCompraSerializer
+
+    def get_serializer_context(self):
+        context = super().get_serializer_context()
+        context.update({"request": self.request})
+        return context
+
+
+class UserCreate(generics.CreateAPIView):
+    queryset = get_user_model().objects.all()
+    serializer_class = UserSerializer
+
+
+class UserDetail(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        user = request.user
+        serializer = UserSerializer(user)
+        return Response(serializer.data)
